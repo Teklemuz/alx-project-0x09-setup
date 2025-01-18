d994e7eef691eac3f317517c5f931154d8cbfde2
Teklemuz Ayenw <111185891+Teklemuz@users.noreply.github.com>
2025-01-18 12:33:15 +0300

    Update index.ts

--- alx-project-0x09/interfaces/index.ts
+++ alx-project-0x09/interfaces/index.ts
@@ -12,8 +12,4 @@ export interface GeneratedImageProps {
   action: (imagePath: string) => void;
 }
 
-export type RequestProps = {
-  prompt: string;
-};
-
 export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">;
